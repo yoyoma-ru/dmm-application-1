e4de31e7c8881834a1432d2647c791ffc3a46dbb
愿山聖 <asjstksmr@gmail.com>
2020-09-26 13:38:15 +0900

    index,createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,35 @@
 class BooksController < ApplicationController
-  def new
+
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-  end
-
-  def index
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
