938500b83fa1a8d8fc35eb03e09ce6299577d6c5
愿山聖 <asjstksmr@gmail.com>
2020-09-26 14:42:41 +0900

    edit,destroy,show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      redirect_to book_path(book), notice: 'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def show
@@ -20,12 +23,21 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path, notice: "Book was successfully destroyed."
+    else
+      render :index
+    end
   end
 
   private
